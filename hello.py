0c3643783796516f26dfe1f4088c64074d2e4ed2
Sharada Prasad <sbermola@hotmail.com>
2024-11-20 12:40:59 +0530

    Hello for class

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-import pandas
-print "Hello"
-print "From Online Editor"
-print "from local"
+class Greeter:
+    def __init__(self, name="World"):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, {self.name}!")
+
+# Create an instance of the Greeter class
+hello_world = Greeter()
+
+# Call the greet method
+hello_world.greet()
